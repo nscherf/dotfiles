b5ade79a4cac1dfb854c2e778e430208ccb0df21
nscherf <nscherf@cbs.mpg.de>
2022-09-25 09:44:11 +0200

    disable ivy and use fido instead (see Mastering Emacs)

--- init.el
+++ init.el
@@ -39,7 +39,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(doom-modeline ivy use-package)))
+ '(fido-mode t)
+ '(fido-vertical-mode t)
+ ;; if you want to use ivy, put ivy back to the list of selected packages
+ '(package-selected-packages '(doom-modeline use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -50,22 +53,22 @@
 
 (use-package command-log-mode)
 
-(use-package ivy
-  :bind (("C-s" . swiper)
-	 :map ivy-minibuffer-map
-	 ("TAB" . ivy-alt-done)
-	 ("C-l" . ivy-alt-done)
-	 ("C-j" . ivy-next-line)
-	 ("C-k" . ivy-previous-line)
-	 :map ivy-switch-buffer-map
-	 ("C-k" . ivy-previous-line)
-	 ("C-l" . ivy-done)
-	 ("C-d" . ivy-switch-buffer-kill)
-	 :map ivy-reverse-i-search-map
-	 ("C-k" . ivy-previous-line)
-	 ("C-d" . ivy-reverse-i-search-kill))
-  :config
-  (ivy-mode 1))
+;; (use-package ivy
+;;  :bind (("C-s" . swiper)
+;;	 :map ivy-minibuffer-map
+;;	 ("TAB" . ivy-alt-done)
+;;	 ("C-l" . ivy-alt-done)
+;;	 ("C-j" . ivy-next-line)
+;;	 ("C-k" . ivy-previous-line)
+;;	 :map ivy-switch-buffer-map
+;;	 ("C-k" . ivy-previous-line)
+;;	 ("C-l" . ivy-done)
+;;	 ("C-d" . ivy-switch-buffer-kill)
+;;	 :map ivy-reverse-i-search-map
+;;	 ("C-k" . ivy-previous-line)
+;;	 ("C-d" . ivy-reverse-i-search-kill))
+;;  :config
+;;  (ivy-mode 1))
 
 (use-package doom-modeline
   :ensure t
@@ -79,3 +82,8 @@
 		term-mode hook
 		eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;; globally replace list-buffers with the more extensive ibuffer when using C-x C-b
+(global-set-key [remap list-buffers] 'ibuffer)
+
+
